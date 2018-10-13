27cab94b0db7276175cd14a6c6e74245ea1c4ead
Chris <chrisvass1@gmail.com>
2018-10-13 15:38:12 -0400

    yuh(1)

--- main.py
+++ main.py
@@ -3,54 +3,48 @@ import mido
 from functools import reduce
 import random
 import math
+import numpy as np
 
-print('check1')
-mid = mido.MidiFile('testriver.mid')
-print(mid)
-#print(mid)
-note_prob = []
+mid = mido.MidiFile('train.mid')
+notes = []
 for x in range(128):
-    note_prob.append([])
+    notes.append([])
     for y in range(128):
-        note_prob[x].append(0)
+        notes[x].append(0)
 # for i, track in enumerate(mid.tracks):
 #     print('Track {}: {}'.format(i, track.name))
 #     for message in track:
 #         print(message)
-print(note_prob)
+print(notes)
 note_ons = []
 
 #TODO use note off eventually
-for i, message in enumerate(mid.tracks[0]):
+for i, message in enumerate(mid.tracks[2]):
     if message.type == "note_on":
             note_ons.append(message)
 
 for i, message in enumerate(note_ons):
     if i < len(note_ons)-1:
-        note_prob[message.note][note_ons[i + 1].note] += 1
+        notes[message.note][note_ons[i + 1].note] += 1
 
-for problist in note_prob:
-    for i, prob in enumerate(problist):
-        problist[i] = math.pow(prob, 1/6)
-    print(problist)
+for next_notes in notes:
+    for i, next_note_prob in enumerate(next_notes):
+        next_notes[i] = math.pow(next_note_prob, 1/6)
 
-for problist in note_prob:
-    for i, prob in enumerate(problist):
-        if reduce((lambda x, y: x+y), problist) != 0:
-            problist[i] = prob / reduce((lambda x, y: x+y), problist)
-
+for i, next_notes in enumerate(notes):
+    total = reduce((lambda x, y: x + y), next_notes)
+    for j, next_note_prob in enumerate(next_notes):
+        if total != 0 and next_note_prob != 0:
+            notes[i][j] = (next_note_prob / total)
 
 
 currentNote = random.randint(0,127)
-while reduce((lambda x, y: x+y), note_prob[currentNote]) == 0:
+while reduce((lambda x, y: x+y), notes[currentNote]) == 0:
     currentNote = random.randint(0,127)
+    print('kill me')
 
-for i in range(1000):
-    playMidi(currentNote, 250 /1000)
+for x in range(1000):
+    playMidi(currentNote, 150 /1000)
     print(currentNote)
-    for j, k in enumerate(note_prob[currentNote]):
-        if random.randint(0,100)/100 < note_prob[currentNote][j]:
-            currentNote = j
-
-
-
+    temp = np.random.choice(128, 1, p=notes[currentNote])
+    currentNote = int(temp)
